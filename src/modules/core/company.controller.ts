d16596c99d969e72eb67f9dde7c8966ddb7f38fd
sandeepsecom <sandeepsecom@gmail.com>
2026-03-03 09:24:24 +0530

    Fix CompanyController export

--- src/modules/core/company.controller.ts
+++ src/modules/core/company.controller.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
-import { SequenceService } from './sequences/sequence.service';
-import { CompanyController } from './company.controller';
-import { PrismaModule } from '../../common/prisma/prisma.module';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { CurrentUser } from '../../common/decorators';
+import { PrismaService } from '../../common/prisma/prisma.service';
 
-@Module({
-  imports: [PrismaModule],
-  controllers: [CompanyController],
-  providers: [SequenceService],
-  exports: [SequenceService],
-})
-export class CoreModule {}
+@Controller('core/company')
+@UseGuards(JwtAuthGuard)
+export class CompanyController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async getMyCompany(@CurrentUser() user: any) {
+    const company = await this.prisma.company.findUnique({
+      where: { id: user.activeCompanyId },
+    });
+    return { data: company };
+  }
+}
