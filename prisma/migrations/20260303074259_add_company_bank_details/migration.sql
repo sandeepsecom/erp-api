ebae21466ea38ef38a91332805454b1cc707fa23
sandeepsecom <sandeepsecom@gmail.com>
2026-03-03 13:13:40 +0530

    Add bank details to company model

---
+++ prisma/migrations/20260303074259_add_company_bank_details/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "companies" ADD COLUMN     "bankAccountName" TEXT,
+ADD COLUMN     "bankAccountNumber" TEXT,
+ADD COLUMN     "bankBranch" TEXT,
+ADD COLUMN     "bankIfsc" TEXT,
+ADD COLUMN     "bankName" TEXT;
