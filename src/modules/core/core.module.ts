131aef76a23455d3130698401edb8cc463ddce57
sandeepsecom <sandeepsecom@gmail.com>
2026-03-03 09:31:43 +0530

    Register CompanyController in CoreModule

--- src/modules/core/core.module.ts
+++ src/modules/core/core.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SequenceService } from './sequences/sequence.service';
+import { CompanyController } from './company.controller';
+import { PrismaModule } from '../../common/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
+  controllers: [CompanyController],
   providers: [SequenceService],
   exports: [SequenceService],
 })
